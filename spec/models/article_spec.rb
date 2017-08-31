7475ee9dcffeece59a52032f523f80a91fc520f2
Ken Murakami <headiv94@gmail.com>
2017-09-01 02:38:51 +0900

    annotate適用 投稿一覧の投稿本文が長い場合に部分表示にとどめる 投稿詳細ページで、投稿本文の改行が正常に表現されていなかったので修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  text               :text
+#  application_period :datetime
+#  capacity           :integer
+#  venue              :string
+#  budget             :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  event_date         :datetime
+#  user_id            :integer
+#  prefecture_id      :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  text               :text
+#  application_period :datetime
+#  capacity           :integer
+#  venue              :string
+#  budget             :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  event_date         :datetime
+#  user_id            :integer
+#  prefecture_id      :integer
+#
+
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
