ea541e7e2282c5ad6af3cd60ffbcc32f8f50282d
Ken Murakami <headiv94@gmail.com>
2017-09-01 02:36:37 +0900

    UserモデルとArticleモデルにカラムを追加

---
+++ db/migrate/20170831172651_add_several_columns_to_user.rb
@@ -0,0 +1,7 @@
+class AddSeveralColumnsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :introduction, :text
+    add_column :users, :sex, :string
+    add_column :users, :birthday, :date
+  end
+end
