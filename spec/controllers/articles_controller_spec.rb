5aba79f4ab1e092d04222d341803a7d21e59a9be
Ken Murakami <headiv94@gmail.com>
2017-09-02 15:01:00 +0900

    ArticlesControllerSpecのリファクタリング shared_examplesを入れるための専用フォルダを作成

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_examples/controllers/articles_controller_spec_shared_examples' # spec内で使われてるshared_examplesはこのファイル内で定義
 
 RSpec.describe ArticlesController, type: :controller do
   context 'ログイン済み' do
@@ -6,121 +7,29 @@ RSpec.describe ArticlesController, type: :controller do
     before { sign_in user }
 
     describe 'GET #index' do
-      context 'レスポンス' do
-        it 'returns http success' do
-          get :index
-          expect(response).to have_http_status(:success)
-        end
-      end
-      context 'データ' do
-        before do
-          create(:article, title: 'カラオケオフ会開催決定！')
-          create(:article, title: '今日一緒にカラオケしませんか？')
-        end
-        it '期待するデータが返ってくるか（タイトル）' do
-          get :index
-          expect(assigns(:articles).pluck(:title)).to match_array(['カラオケオフ会開催決定！', '今日一緒にカラオケしませんか？'])
-        end
-        it '期待するデータが返ってくるか（個数）' do
-          get :index
-          expect(assigns(:articles).count).to eq 2
-        end
-      end
+      it_behaves_like 'indexアクション正常系'
     end
 
     describe 'GET #show' do
-      let(:article) {create(:article)}
-      let(:params) {{id: article}}
-      before do
-        get :show, params: params
-      end
-      context "レスポンス" do
-        it "returns http success" do
-          expect(response).to have_http_status(:success)
-        end
-        it 'インスタンス変数に指定IDのArticleを含むこと' do
-          expect(assigns[:article]).to eq article
-        end
-        it 'showテンプレートがrenderされていること' do
-          expect(response).to render_template(:show)
-        end
-      end
+      it_behaves_like 'showアクション正常系'
     end
 
     describe 'GET #new' do
-      context 'レスポンス' do
-        it 'returns http success' do
-          get :new
-          expect(response).to have_http_status(:success)
-        end
-        it 'インスタンス変数に空のArticleが含まれていること' do
-          get :new
-          expect(assigns[:article]).to be_a_new(Article)
-        end
-        it 'newテンプレートがrenderされていること' do
-          get :new
-          expect(response).to render_template(:new)
-        end
-      end
+      it_behaves_like 'newアクション正常系'
     end
   end
 
   context '未ログイン' do
     describe 'GET #index' do
-      context 'レスポンス' do
-        it 'returns http success' do
-          get :index
-          expect(response).to have_http_status(:success)
-        end
-      end
-
-      context 'データ' do
-        before do
-          create(:article, title: 'カラオケオフ会開催決定！')
-          create(:article, title: '今日一緒にカラオケしませんか？')
-        end
-        it '期待するデータが返ってくるか（タイトル）' do
-          get :index
-          expect(assigns(:articles).pluck(:title)).to match_array(['カラオケオフ会開催決定！', '今日一緒にカラオケしませんか？'])
-        end
-        it '期待するデータが返ってくるか（個数）' do
-          get :index
-          expect(assigns(:articles).count).to eq 2
-        end
-      end
+      it_behaves_like 'indexアクション正常系'
     end
 
     describe 'GET #show' do
-      let(:article) {create(:article)}
-      let(:params) {{id: article}}
-      before do
-        get :show, params: params
-      end
-      context "レスポンス" do
-        it "returns http success" do
-          expect(response).to have_http_status(:success)
-        end
-        it 'インスタンス変数に指定IDのArticleを含むこと' do
-          expect(assigns[:article]).to eq article
-        end
-        it 'showテンプレートがrenderされていること' do
-          expect(response).to render_template(:show)
-        end
-      end
+      it_behaves_like 'showアクション正常系'
     end
 
     describe 'GET #new' do
-      context 'レスポンス' do
-        before do
-          get :new
-        end
-        it "returns http success" do
-          expect(response).to have_http_status(302)
-        end
-        it "ログインページにリダイレクトされること" do
-          expect(response).to redirect_to new_user_session_path
-        end
-      end
+      it_behaves_like 'newアクション異常系（未ログイン時の処理）'
     end
   end
 end
