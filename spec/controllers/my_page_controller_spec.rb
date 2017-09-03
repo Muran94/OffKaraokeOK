3b386d4727481980711ea67eb674af47c58c3740
Ken Murakami <headiv94@gmail.com>
2017-09-04 00:07:52 +0900

    MyPageコントローラspecの修正 ParticipantControllerSpecの修正

--- spec/controllers/my_page_controller_spec.rb
+++ spec/controllers/my_page_controller_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe MyPageController, type: :controller do
+  context "ログイン中" do
+    let(:user) {create(:user)}
+    before do
+      sign_in user
+    end
 
-  describe "GET #profile" do
-    it "returns http success" do
-      get :profile
-      expect(response).to have_http_status(:success)
+    describe "GET #profile" do
+      it "returns http success" do
+        get :profile
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "GET #participating_event" do
+      it "returns http success" do
+        get :participating_event
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  describe "GET #participating_event" do
-    it "returns http success" do
-      get :participating_event
-      expect(response).to have_http_status(:success)
-    end
-  end
+  context "未ログイン" do
 
+  end
 end
