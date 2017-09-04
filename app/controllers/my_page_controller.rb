a614908cad519626e12bfc4ee00fa8ade70b0afa
Ken Murakami <headiv94@gmail.com>
2017-09-04 23:12:14 +0900

    マイページの整理

--- app/controllers/my_page_controller.rb
+++ app/controllers/my_page_controller.rb
@@ -2,9 +2,6 @@ class MyPageController < ApplicationController
   before_action :authenticate_user!
 
   def profile
-  end
-
-  def participating_event
     @participating_events = current_user.participants
   end
 end
