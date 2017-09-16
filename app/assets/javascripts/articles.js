f77f2ed0a51d6eb97ef990f75c68b71050fbfd5c
Ken Murakami <headiv94@gmail.com>
2017-09-17 04:29:53 +0900

    コメント追加 & 投稿オーナーの投稿詳細プルダウンのメニューバーのデザイン調整

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,10 +1,12 @@
 $(document).on('turbolinks:load', function() {
   // 投稿リストの開閉処理
+  // 投稿リストのタイトルをクリックした時に詳細をプルダウンする
   $(".js-article-header").click(function() {
     $(".js-article-detail").not($(this).next()).slideUp();
     $(this).next().slideToggle();
   })
 
+  // 投稿詳細プルダウンのメニューバーに表示される「閉じる」ボタンを押した時に詳細を閉じる
   $(".js-close-article-detail").click(function() {
     $(this).closest('.js-article-detail').slideUp();
   })
