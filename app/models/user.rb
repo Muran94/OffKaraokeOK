3d64301fb696a5451de745b15c96eea470f1f1f5
Ken Murakami <headiv94@gmail.com>
2017-09-22 21:59:05 +0900

    ログイン回数の条件を５回に設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :image, ProfileImageUploader
