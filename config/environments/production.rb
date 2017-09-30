91b3a187e6f7c1a810ada2c823f135f6bdf44438
Muran94 <headiv94@gmail.com>
2017-09-30 17:19:58 +0900

    メールのデザイン修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
+  # メール内にcssを適用するための設定
+  config.action_controller.asset_host = 'http://offkara.jp'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   GA.tracker = 'UA-107239870-1'
 
