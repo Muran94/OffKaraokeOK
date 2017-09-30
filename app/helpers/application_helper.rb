2edd481a27f16d3691b6f6d63e8b7c81219ee2cb
Muran94 <headiv94@gmail.com>
2017-10-01 00:19:29 +0900

    レスポンシブ対応 ヘッダータブのactive実装 細かい部分の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def current_page_article_index_page?
+    controller.controller_name == "articles" && controller.action_name == "index"
+  end
+
+  def current_page_article_new_page?
+    controller.controller_name == "articles" && controller.action_name == "new"
+  end
+
+  def current_page_my_page?
+    controller.controller_name == "my_page"
+  end
+
+  def current_page_user_registration_page?
+    controller.controller_name == "registrations" && controller.action_name == "new"
+  end
+
+  def current_page_user_login_page?
+    controller.controller_name == "sessions" && controller.action_name == "new"
+  end
 end
