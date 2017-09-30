b94f8ba45376a03eb89e9e8458427119aaf4495b
Muran94 <headiv94@gmail.com>
2017-10-01 01:05:31 +0900

    スマホのメニューバーの表示不具合修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@
 (function($){
   $(document).on('turbolinks:load', function() {
     $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
+      menuWidth: 150, // Default is 300
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens,
